930215f811645c3320d3acf932e0a87a74d8bcc3
Jacob Klenner <jacobryce@gmail.com>
2022-02-06 20:19:23 +0100

    add basic sql query builder functionality

--- sql/sql.go
+++ sql/sql.go
@@ -1,1 +1,136 @@
 package sql
+
+import (
+	"fmt"
+	"strings"
+)
+
+// dont think this should be called 'query' as it contains more information than just a simple query
+type Query struct {
+	// Language string // should support syntactic differences in mysql, postgres, ms sql etc
+	// Version string // may be differences in symbols based on versions
+	Database string
+	Table    string
+	Columns  Columns // TODO add support for table meta data, and can do some validation on the built query
+	Query    string
+}
+
+type Columns map[string]Column
+
+type Column struct {
+	Name string
+	// DataType string // TODO could add some data type validation
+}
+
+// name these based on what reads well when constructing? or
+func (c Column) Equal(v string) string {
+	return fmt.Sprintf("%s = %s", c.Name, v)
+}
+
+func (c Column) NotEqual(v string) string {
+	return fmt.Sprintf("%s <> %s", c.Name, v)
+}
+
+func (c Column) GreaterThan(v string) string {
+	return fmt.Sprintf("%s > %s", c.Name, v)
+}
+
+func (c Column) GreaterThanOrEqual(v string) string {
+	return fmt.Sprintf("%s >= %s", c.Name, v)
+}
+
+func (c Column) LessThan(v string) string {
+	return fmt.Sprintf("%s < %s", c.Name, v)
+}
+
+func (c Column) LessThanOrEqual(v string) string {
+	return fmt.Sprintf("%s <= %s", c.Name, v)
+}
+
+func (c Column) Like(p string) string {
+	return fmt.Sprintf("%s LIKE %s", c.Name, p)
+}
+
+func (c Column) NotLike(p string) string {
+	return fmt.Sprintf("%s NOT LIKE %s", c.Name, p)
+}
+
+func (c Column) Between(l string, u string) string {
+	return fmt.Sprintf("%s BETWEEN %s AND %s", c.Name, l, u)
+}
+
+func (c Column) In(r string) string {
+	return fmt.Sprintf("%s IN (%s)", c.Name, r)
+}
+
+func (c Column) NotIn(r string) string {
+	return fmt.Sprintf("%s NOT IN (%s)", c.Name, r)
+}
+
+func (q *Query) Select(cols []string) *Query {
+	var s string
+
+	// do we expect this as default behaviour?
+	if len(cols) == 0 {
+		s = "*"
+	} else {
+		for _, c := range cols {
+			s = s + fmt.Sprintf("%s, ", c)
+		}
+		s = strings.TrimSuffix(s, ", ")
+	}
+
+	q.Query = fmt.Sprintf("SELECT %s FROM %s.%s;", s, q.Database, q.Table)
+	return q
+}
+
+func (q *Query) SelectOne() *Query {
+	q.Query = fmt.Sprintf("SELECT 1 FROM %s.%s;", q.Database, q.Table)
+	return q
+}
+
+func (q *Query) SelectAll() *Query {
+	q.Query = fmt.Sprintf("SELECT * FROM %s.%s;", q.Database, q.Table)
+	return q
+}
+
+func (q *Query) Where(cond string) *Query {
+	q.Query = strings.TrimSuffix(q.Query, ";")
+	q.Query = fmt.Sprintf("%s WHERE %s;", q.Query, cond)
+
+	return q
+}
+
+func (q *Query) And(cond string) *Query {
+	q.Query = strings.TrimSuffix(q.Query, ";")
+	q.Query = fmt.Sprintf("%s AND %s;", q.Query, cond)
+
+	return q
+}
+
+func (q *Query) Or(cond string) *Query {
+	q.Query = strings.TrimSuffix(q.Query, ";")
+	q.Query = fmt.Sprintf("%s OR %s;", q.Query, cond)
+
+	return q
+}
+
+func (q *Query) Limit(val int) *Query {
+	q.Query = strings.TrimSuffix(q.Query, ";")
+	q.Query = fmt.Sprintf("%s LIMIT(%d);", q.Query, val)
+
+	return q
+}
+
+func (q *Query) OrderByAsc(col Column) *Query {
+	q.Query = strings.TrimSuffix(q.Query, ";")
+	q.Query = fmt.Sprintf("%s ORDER BY %s ASC;", q.Query, col.Name)
+
+	return q
+}
+
+func (q *Query) OrderByDesc(col Column) *Query {
+	q.Query = strings.TrimSuffix(q.Query, ";")
+	q.Query = fmt.Sprintf("%s ORDER BY %s DESC;", q.Query, col.Name)
+	return q
+}
--- sql/sql_test.go
+++ sql/sql_test.go
@@ -1,7 +1,143 @@
 package sql
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestSql(t *testing.T) {
+func assert(t *testing.T, exp string, res string) {
+	if exp != res {
+		t.Logf("expected %s, got %s", exp, res)
+		t.Fail()
+	}
+}
 
+func getTestQuery() Query {
+
+	cols := map[string]Column{
+		"account_id": {
+			Name: "account_id",
+		},
+		"value": {
+			Name: "value",
+		},
+		"status": {
+			Name: "status",
+		},
+		"created_at": {
+			Name: "created_at",
+		},
+	}
+
+	return Query{
+		Database: "client_db",
+		Table:    "accounts",
+		Columns:  cols,
+	}
+}
+
+func TestEmptySelect(t *testing.T) {
+	q := getTestQuery()
+
+	q.Select([]string{})
+
+	exp := fmt.Sprintf("SELECT * FROM %s.%s;", q.Database, q.Table)
+
+	assert(t, exp, q.Query)
+}
+
+func TestSingleSelect(t *testing.T) {
+	q := getTestQuery()
+
+	q.Select([]string{"account_id"})
+
+	exp := fmt.Sprintf("SELECT account_id FROM %s.%s;", q.Database, q.Table)
+
+	assert(t, exp, q.Query)
+}
+
+func TestDoubleSelect(t *testing.T) {
+	q := getTestQuery()
+
+	sel := []string{
+		"account_id",
+		"value",
+	}
+
+	q.Select(sel)
+
+	exp := fmt.Sprintf("SELECT account_id, value FROM %s.%s;", q.Database, q.Table)
+
+	assert(t, exp, q.Query)
+}
+
+func TestSelectOne(t *testing.T) {
+	q := getTestQuery()
+
+	q.SelectOne()
+
+	exp := fmt.Sprintf("SELECT 1 FROM %s.%s;", q.Database, q.Table)
+
+	assert(t, exp, q.Query)
+}
+
+func TestSelectAll(t *testing.T) {
+	q := getTestQuery()
+
+	q.SelectAll()
+
+	exp := fmt.Sprintf("SELECT * FROM %s.%s;", q.Database, q.Table)
+
+	assert(t, exp, q.Query)
+}
+
+func TestEqualVarchar(t *testing.T) {
+	col := Column{Name: "account_id"}
+	res := col.Equal("'acc-001'")
+	exp := "account_id = 'acc-001'"
+	assert(t, exp, res)
+}
+
+func TestEqualNum(t *testing.T) {
+	col := Column{Name: "account_id"}
+	res := col.Equal("5")
+	exp := "account_id = 5"
+	assert(t, exp, res)
+}
+
+func TestNotEqual(t *testing.T) {
+	col := Column{Name: "account_id"}
+	res := col.NotEqual("'acc-001'")
+	exp := "account_id <> 'acc-001'"
+	assert(t, exp, res)
+}
+
+func TestWhereEquals(t *testing.T) {
+	q := getTestQuery()
+
+	q.SelectAll().Where(q.Columns["account_id"].Equal("'acc-001'"))
+
+	exp := fmt.Sprintf("SELECT * FROM %s.%s WHERE account_id = 'acc-001';", q.Database, q.Table)
+
+	assert(t, exp, q.Query)
+}
+
+func TestWhereLikeAndBetweenOrderByAsc(t *testing.T) {
+	q := getTestQuery()
+	like := `'acc-1%'`
+	q.SelectAll().Where(q.Columns["account_id"].Like(like)).And(q.Columns["value"].Between(`500`, `1000`)).OrderByAsc(q.Columns["created_at"])
+
+	exp := fmt.Sprintf("SELECT * FROM %s.%s WHERE account_id LIKE %s AND value BETWEEN 500 AND 1000 ORDER BY created_at ASC;", q.Database, q.Table, like)
+
+	assert(t, exp, q.Query)
+}
+
+func TestWhereInOrNotEqualOrderByDesc(t *testing.T) {
+	q := getTestQuery()
+
+	q.SelectAll().Where(q.Columns["status"].In(`'failed','deleted'`)).Or(q.Columns["value"].NotEqual(`100`)).OrderByDesc(q.Columns["created_at"])
+
+	exp := fmt.Sprintf("SELECT * FROM %s.%s WHERE status IN ('failed','deleted') OR value <> 100 ORDER BY created_at DESC;", q.Database, q.Table)
+
+	assert(t, exp, q.Query)
 }
